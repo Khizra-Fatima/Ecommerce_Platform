7a74406b4c5e6ff6af23fa55fad76e7753ef5a46
Khizra-Fatima <khizrafatimaqureshi369@gmail.com>
2025-08-13 10:07:21 +0500

    Edit Requirements file

--- Ecommerce_Platform/settings.py
+++ Ecommerce_Platform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'local-dev-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
