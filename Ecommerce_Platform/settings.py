8405b9aaea780c32086d17f6848553fe35481cf5
Khizra-Fatima <khizrafatimaqureshi369@gmail.com>
2025-09-09 15:10:26 +0500

    Removed Reference line of css.map file

--- Ecommerce_Platform/settings.py
+++ Ecommerce_Platform/settings.py
@@ -186,7 +186,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STORAGES = {
     "staticfiles": {
