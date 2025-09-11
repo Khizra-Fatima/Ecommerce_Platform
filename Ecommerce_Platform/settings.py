5800929e9afe89a40d6bbbccc861943f605e806d
Khizra-Fatima <khizrafatimaqureshi369@gmail.com>
2025-09-11 18:51:31 +0500

    Updated settings.py

--- Ecommerce_Platform/settings.py
+++ Ecommerce_Platform/settings.py
@@ -186,7 +186,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STORAGES = {
