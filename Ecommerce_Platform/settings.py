ad1adb0707d270fd00f83dd31d1667c80e8447ba
Khizra-Fatima <khizrafatimaqureshi369@gmail.com>
2025-09-15 10:21:23 +0500

    Updated settings.py

--- Ecommerce_Platform/settings.py
+++ Ecommerce_Platform/settings.py
@@ -199,7 +199,7 @@ STORAGES = {
 STATICFILES_STORAGE = STORAGES["staticfiles"]["BACKEND"]
 
 
-WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ["map", "css", "js", "md", "txt", "png", "jpg", "jpeg", "gif", "ico", "woff", "woff2", "ttf", "eot", "svg"]
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ["map", "css", "js", "md", "txt", "png", "jpg", "jpeg", "gif", "ico", "woff", "woff2", "ttf", "eot", "svg", "LICENSE", "license" ]
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
